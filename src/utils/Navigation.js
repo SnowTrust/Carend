a15c116f04c88e81347a131d497482db6fafe765
itishermann <hello@itishermann.me>
2020-11-12 18:26:55 +0100

    final touches

--- src/utils/Navigation.js
+++ src/utils/Navigation.js
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 
 const Navigation = () => {
     return (
-        <Stack.Navigator initialRouteName="Write" headerMode='none'>
+        <Stack.Navigator initialRouteName="Home" headerMode='none'>
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="Write" component={Write} />
             <Stack.Screen name="Welcome" component={Welcome} />
